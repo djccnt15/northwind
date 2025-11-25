85c0ed0b5210e822835ff0fb8366c2b076014fc2
Jay.D <djccnt15@gmail.com>
2025-11-25 23:03:48 +0900

    create `LogInterceptor` for logging request, response

---
+++ src/main/java/com/djccnt15/northwind/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.djccnt15.northwind.config;
+
+import com.djccnt15.northwind.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry
+            .addInterceptor(new LogInterceptor())
+            .order(1)
+            .addPathPatterns("/**")
+            .excludePathPatterns(
+                "/css/**", "/*.ico",
+                "/error", "/error-page/**"  // 에러 발생 -> 에러 페이지 호출 시의 인터셉터 중복 호출 제거
+            )
+        ;
+    }
+}
