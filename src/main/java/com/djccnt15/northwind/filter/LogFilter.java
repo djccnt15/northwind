7f794a5c9ed3da7dc5e8e6dee191defd42d374f5
Jay.D <djccnt15@gmail.com>
2025-11-25 21:43:47 +0900

    create `LogFilter` for log tracing

---
+++ src/main/java/com/djccnt15/northwind/filter/LogFilter.java
@@ -0,0 +1,47 @@
+package com.djccnt15.northwind.filter;
+
+import jakarta.servlet.*;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static java.lang.Integer.MIN_VALUE;
+import static java.util.UUID.randomUUID;
+
+@Slf4j
+@Component
+@Order(value = MIN_VALUE)
+public class LogFilter implements Filter {
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("log filter init");
+    }
+    
+    @Override
+    public void doFilter(
+        ServletRequest request,
+        ServletResponse response,
+        FilterChain chain
+    ) throws IOException, ServletException {
+        // log.info("log filter doFilter");
+        MDC.put("traceId", randomUUID().toString().substring(0, 8));  // store in thread-local MDC
+        
+        try {
+            // log.info("DispatcherType={}", request.getDispatcherType());
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            MDC.clear();  // always clear to avoid leaking across threads
+        }
+    }
+    
+    @Override
+    public void destroy() {
+        log.info("log filter destroy");
+    }
+}
