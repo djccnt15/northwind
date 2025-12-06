8024dc02fc43f142f50bbba82759dce4512a5bda
Jay.D <djccnt15@gmail.com>
2025-12-06 22:42:49 +0900

    create `BaseEntity` for id column

---
+++ src/main/java/com/djccnt15/northwind/db/entity/id/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.djccnt15.northwind.db.entity.id;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@MappedSuperclass  // annotation for abstract table class
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public abstract class BaseEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
