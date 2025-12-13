b4cb63797065397f5fd91ecb499ab0f9fa5411c4
Jay.D <djccnt15@gmail.com>
2025-12-13 23:46:16 +0900

    create `AuthConfig` for security configuration

---
+++ src/main/java/com/djccnt15/northwind/config/security/AuthConfig.java
@@ -0,0 +1,19 @@
+package com.djccnt15.northwind.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AuthConfig {
+    
+    /**
+     * Bean for password encoding
+     */
+    @Bean
+    // make returned object as spring bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
