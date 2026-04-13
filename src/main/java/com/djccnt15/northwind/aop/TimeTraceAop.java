251b2ee1a2b4a114bc604e030857cddee5127397
Jay.D <djccnt15@gmail.com>
2026-04-14 00:25:06 +0900

    update `TimeTraceAop` for concise time tracing

--- src/main/java/com/djccnt15/northwind/aop/TimeTraceAop.java
+++ src/main/java/com/djccnt15/northwind/aop/TimeTraceAop.java
@@ -11,16 +11,36 @@ import org.springframework.stereotype.Component;
 @Component
 public class TimeTraceAop {
     
-    @Around("execution(* com.djccnt15.northwind..*(..))")
-    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.info("START: {}", joinPoint);
+    @Around("""
+        execution(* com.djccnt15.northwind.domain..*(..))
+        && !@within(com.djccnt15.northwind.annotation.Converter)
+        && (@within(com.djccnt15.northwind.annotation.Business)
+        || @within(org.springframework.stereotype.Service))
+        """)
+    public Object executeBusiness(ProceedingJoinPoint joinPoint) throws Throwable {
+        // log.info("START: {}", joinPoint);
         var start = System.currentTimeMillis();
         try {
             return joinPoint.proceed();
         } finally {
             var end = System.currentTimeMillis();
             var timeMs = end - start;
-            log.info("END: {} {}ms", joinPoint, timeMs);
+            // log.info("END: {} {}ms", joinPoint, timeMs);
+            log.info("{}:  {}ms", joinPoint, timeMs);
+        }
+    }
+    
+    @Around("execution(* com.djccnt15.northwind.db.repository..*(..))")
+    public Object executeSql(ProceedingJoinPoint joinPoint) throws Throwable {
+        // log.info("START: {}", joinPoint);
+        var start = System.currentTimeMillis();
+        try {
+            return joinPoint.proceed();
+        } finally {
+            var end = System.currentTimeMillis();
+            var timeMs = end - start;
+            // log.info("END: {} {}ms", joinPoint, timeMs);
+            log.info("{}:  {}ms", joinPoint, timeMs);
         }
     }
 }
