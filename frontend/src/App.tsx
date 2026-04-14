7980a7553431ecfcba8349260cc295be0eb2d8f8
Jay.D <djccnt15@gmail.com>
2026-04-14 23:46:57 +0900

    create GlobalStyle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,37 @@
+import styled, { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+
+  * {
+    box-sizing: border-box;
+  };
+
+  body {
+    background-color: white;
+    color: black;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  };
+
+  ::-webkit-scrollbar {
+    display:none;
+  }
+`;
+
 function App() {
-  return <>Hello World</>;
+  return (
+    <Wrapper>
+      <GlobalStyles />
+      <>Hello World</>
+    </Wrapper>
+  );
 }
 
 export default App;
