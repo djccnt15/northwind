cc1882ddd24c85d83aece1311fa8998cdc2a8c08
Jay.D <djccnt15@gmail.com>
2026-04-16 23:17:57 +0900

    create frontend login function

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 import { CommBorderRadius } from "../shared/boder";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding-bottom: 200px;
+  padding-bottom: 50px;
 `;
 
 const H1 = styled.h1`
@@ -20,19 +23,23 @@ const Form = styled.form`
   flex-direction: column;
   align-items: center;
   gap: 15px;
+  width: 250px;
+  margin-bottom: 20px;
 `;
 
 const Input = styled.input`
   ${CommBorderRadius}
   padding: 10px;
-  width: 200px;
+  width: 100%;
   border: 1px solid #ccc;
   font-size: 16px;
+  height: 40px;
 `;
 
-const Button = styled.button`
+const SubmitBtn = styled.input`
   ${CommBorderRadius}
   width: 100%;
+  height: 40px;
   padding: 10px 20px;
   border: none;
   background-color: #17c1ff;
@@ -44,15 +51,77 @@ const Button = styled.button`
   }
 `;
 
+const ErrorMsg = styled.span`
+  color: tomato;
+  font-size: 16px;
+  font-weight: 600;
+`;
+
 export default function Login() {
+  const navigate = useNavigate();
+
+  const [errorMsg, setErrorMsg] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const onChangeUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const onSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setErrorMsg("");
+    setIsLoading(true);
+
+    const params = new URLSearchParams();
+    params.append("username", username);
+    params.append("password", password);
+
+    axios
+      .post("/api/login", params, {
+        headers: { "content-type": "application/x-www-form-urlencoded" },
+      })
+      .then(() => {
+        navigate("/");
+      })
+      .catch(() => {
+        setErrorMsg("Login failed. Please check your username and password.");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
   return (
     <Wrapper>
       <H1>Please Sign In</H1>
-      <Form>
-        <Input type="text" placeholder="Username" />
-        <Input type="password" placeholder="Password" />
-        <Button type="submit">Login</Button>
+      <Form onSubmit={onSubmit}>
+        <Input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={onChangeUsername}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={onChangePassword}
+        />
+        <SubmitBtn
+          type="submit"
+          value={isLoading ? "Loading..." : "Login"}
+          disabled={isLoading}
+        />
       </Form>
+      {errorMsg && <ErrorMsg>{errorMsg}</ErrorMsg>}
     </Wrapper>
   );
 }
