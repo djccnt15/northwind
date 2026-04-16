5f1faee8bb612e91c92150b057ba637c6a22e81f
Jay.D <djccnt15@gmail.com>
2026-04-17 00:07:02 +0900

    create `createUserRole`, `createAdmin` for basic date creation

--- src/main/java/com/djccnt15/northwind/db/repository/UserRoleRepo.java
+++ src/main/java/com/djccnt15/northwind/db/repository/UserRoleRepo.java
@@ -3,5 +3,9 @@ package com.djccnt15.northwind.db.repository;
 import com.djccnt15.northwind.db.entity.UserRoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRoleRepo extends JpaRepository<UserRoleEntity, Long> {
+    
+    Optional<UserRoleEntity> findFirstByName(String name);
 }
--- src/test/java/com/djccnt15/northwind/sampledata/UserCreateTest.java
+++ src/test/java/com/djccnt15/northwind/sampledata/UserCreateTest.java
@@ -1,25 +1,55 @@
 package com.djccnt15.northwind.sampledata;
 
 import com.djccnt15.northwind.db.entity.AppUserEntity;
+import com.djccnt15.northwind.db.entity.AppUserRoleEntity;
+import com.djccnt15.northwind.db.entity.UserRoleEntity;
 import com.djccnt15.northwind.db.repository.AppUserRepo;
+import com.djccnt15.northwind.db.repository.AppUserRoleRepo;
+import com.djccnt15.northwind.db.repository.UserRoleRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.List;
+
 @SpringBootTest
 public class UserCreateTest {
     
     @Autowired private PasswordEncoder encoder;
-    @Autowired private AppUserRepo repository;
+    @Autowired private AppUserRepo userRepo;
+    @Autowired private UserRoleRepo userRoleRepo;
+    @Autowired private AppUserRoleRepo appUserRoleRepo;
+    
+    @Test
+    void createUserRole() {
+        var superAdmin = UserRoleEntity.builder()
+            .name("superadmin")
+            .build();
+        var admin = UserRoleEntity.builder()
+            .name("admin")
+            .build();
+        var user = UserRoleEntity.builder()
+            .name("user")
+            .build();
+        userRoleRepo.saveAll(List.of(superAdmin, admin, user));
+    }
     
     @Test
     void createAdmin() {
-        var admin = AppUserEntity.builder()
+        var superAdmin = AppUserEntity.builder()
             .username("admin")
             .password(encoder.encode("admin"))
             .email("admin@b.com")
+            .isVerified(true)
             .build();
-        repository.save(admin);
+        userRepo.save(superAdmin);
+        
+        var userRole = userRoleRepo.findFirstByName("superadmin").orElseThrow();
+        var appUserRole = AppUserRoleEntity.builder()
+            .appUser(superAdmin)
+            .userRole(userRole)
+            .build();
+        appUserRoleRepo.save(appUserRole);
     }
 }
