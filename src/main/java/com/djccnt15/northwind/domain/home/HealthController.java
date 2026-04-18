73f81673ca50cbe830e2b831c8c9b93958c4e87f
Jay.D <djccnt15@gmail.com>
2026-04-18 15:52:09 +0900

    update `HealthController` to response API request

--- src/main/java/com/djccnt15/northwind/domain/home/HealthController.java
+++ src/main/java/com/djccnt15/northwind/domain/home/HealthController.java
@@ -3,14 +3,21 @@ package com.djccnt15.northwind.domain.home;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequestMapping("/api")
 public class HealthController {
     
     @GetMapping("/health")
     public ResponseEntity<?> health() {
         return ResponseEntity.ok(1);
+    }
+    
+    @GetMapping("/ping")
+    public ResponseEntity<?> ping() {
+        return ResponseEntity.ok("pong");
     }
 }
