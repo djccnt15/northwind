b85bbd7499adf74cfb61987e04946dacd2ae53e8
Jay.D <djccnt15@gmail.com>
2026-04-20 00:39:16 +0900

    create temporal `home` page for protected route

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import { useAuth } from "../shared/auth/auth";
+
+const Wrapper = styled.div``;
+
+const H1 = styled.h1``;
+
+const P = styled.p``;
+
+export default function Home() {
+  const { user } = useAuth();
+
+  return (
+    <Wrapper>
+      <H1>Welcome to Northwind! {user?.username}</H1>
+      <P>
+        This is the home page. Please use the navigation menu to explore the
+        app.
+      </P>
+    </Wrapper>
+  );
+}
