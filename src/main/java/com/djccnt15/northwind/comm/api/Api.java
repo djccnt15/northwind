404da7ef5a6c473043b81ecd291b44b623a28245
Jay.D <djccnt15@gmail.com>
2026-04-23 21:20:13 +0900

    add `serverTime` field to `Api` class

--- src/main/java/com/djccnt15/northwind/comm/api/Api.java
+++ src/main/java/com/djccnt15/northwind/comm/api/Api.java
@@ -6,10 +6,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import static java.lang.System.currentTimeMillis;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Api<T> {
+    
+    private long serverTime;  // Epoch Millis
     
     private Result result;
     
@@ -17,11 +21,11 @@ public class Api<T> {
     private T body;
     
     public static <T> Api<T> OK(T data) {
-        return new Api<>(Result.OK(), data);
+        return new Api<>(currentTimeMillis(), Result.OK(), data);
     }
     
     public static <T> Api<T> CREATED(T data) {
-        return new Api<>(Result.CREATED(), data);
+        return new Api<>(currentTimeMillis(), Result.CREATED(), data);
     }
     
     public static Api<Object> ERROR(Result result) {
@@ -32,18 +36,21 @@ public class Api<T> {
     
     public static Api<Object> ERROR(StatusCodeIfs statusCodeIfs) {
         var api = new Api<Object>();
+        api.serverTime = currentTimeMillis();
         api.result = Result.ERROR(statusCodeIfs);
         return api;
     }
     
     public static Api<Object> ERROR(StatusCodeIfs statusCodeIfs, Throwable tx) {
         var api = new Api<Object>();
+        api.serverTime = currentTimeMillis();
         api.result = Result.ERROR(statusCodeIfs, tx);
         return api;
     }
     
     public static Api<Object> ERROR(StatusCodeIfs statusCodeIfs, String description) {
         var api = new Api<Object>();
+        api.serverTime = currentTimeMillis();
         api.result = Result.ERROR(statusCodeIfs, description);
         return api;
     }
