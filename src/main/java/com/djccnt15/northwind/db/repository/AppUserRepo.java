66516124c5563aa80a1108757acf52e23ddb393a
Jay.D <djccnt15@gmail.com>
2026-04-25 01:30:09 +0900

    use `@EntityGraph` to fix N+1 issue of `findFirstByUsername` query

--- src/main/java/com/djccnt15/northwind/db/repository/AppUserRepo.java
+++ src/main/java/com/djccnt15/northwind/db/repository/AppUserRepo.java
@@ -1,11 +1,13 @@
 package com.djccnt15.northwind.db.repository;
 
 import com.djccnt15.northwind.db.entity.AppUserEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface AppUserRepo extends JpaRepository<AppUserEntity, Long> {
     
+    @EntityGraph(attributePaths = {"appUserRole", "appUserRole.userRole"})
     Optional<AppUserEntity> findFirstByUsername(String username);
 }
