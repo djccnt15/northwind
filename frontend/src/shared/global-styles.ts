442c9d357e00e7582559eec86d0a9462e35165dc
Jay.D <djccnt15@gmail.com>
2026-04-27 00:07:20 +0900

    create Modal base component

--- frontend/src/shared/global-styles.ts
+++ frontend/src/shared/global-styles.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle, css } from "styled-components";
+import styled, { createGlobalStyle, css } from "styled-components";
 import reset from "styled-reset";
 
 export const GlobalStyles = createGlobalStyle`
@@ -41,4 +41,30 @@ export const commBtnHoverSkyBlue = css`
 
 export const commBtnSkyBlueBoxShadow = css`
   box-shadow: 0 0 0 1px #006885;
+`;
+
+export const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9998;
+`;
+
+export const ModalFadeInAnimation = css`
+  animation: fadeIn 0.5s ease-out forwards;
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
 `;
