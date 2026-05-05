06717b740fd88ea479246a9f9a29294806865a1c
Jay.D <djccnt15@gmail.com>
2026-05-05 23:28:41 +0900

    create `GeneralConfig` for managing general config in one class

--- src/main/java/com/djccnt15/northwind/NorthwindApplication.java
+++ src/main/java/com/djccnt15/northwind/NorthwindApplication.java
@@ -2,10 +2,8 @@ package com.djccnt15.northwind;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan  // make spring scan classes with @ConfigurationProperties annotation
 public class NorthwindApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/djccnt15/northwind/config/GeneralConfig.java
@@ -0,0 +1,9 @@
+package com.djccnt15.northwind.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan  // enable @ConfigurationProperties annotation
+public class GeneralConfig {
+}
