15a00dec422c9fdfd7f74ae41356b4910da775a9
Jay.D <djccnt15@gmail.com>
2026-05-07 22:30:55 +0900

    create `NoResourceFoundException` handler to handle `NOT_FOUND` exception

--- src/main/java/com/djccnt15/northwind/global/exception/advice/GlobalExceptionHandler.java
+++ src/main/java/com/djccnt15/northwind/global/exception/advice/GlobalExceptionHandler.java
@@ -6,13 +6,24 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+import static com.djccnt15.northwind.global.code.StatusCode.NOT_FOUND;
 import static com.djccnt15.northwind.global.code.StatusCode.SERVER_ERROR;
 
 @Slf4j
 @RestControllerAdvice
 @Order(Integer.MAX_VALUE)  // max value is explicit of default
 public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<Api<?>> noResourceFound(NoResourceFoundException exception) {
+        log.error("", exception);
+        
+        return ResponseEntity
+            .status(NOT_FOUND.getHttpStatusCode())
+            .body(Api.ERROR(NOT_FOUND, "Resource not found"));
+    }
     
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Api<?>> exception(Exception exception) {
