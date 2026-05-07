5dd81e2527119327f75ec6491bad15d590ec4b11
Jay.D <djccnt15@gmail.com>
2026-05-07 22:11:14 +0900

    separate `LeftNavBar` component from `layout` create `left-nav-bar.tsx` in widget

---
+++ frontend/src/widgets/left-nav-bar.tsx
@@ -0,0 +1,168 @@
+import styled from "styled-components";
+import { globalTransition } from "../shared/global-styles";
+import { useAuth } from "../shared/auth/auth-context";
+import { Link, useNavigate } from "react-router-dom";
+import { privateApi } from "../shared/api";
+
+const Nav = styled.nav`
+  display: flex;
+  background-color: #efefef;
+  flex-direction: column;
+  align-items: start;
+  min-width: 150px;
+  padding: 20px 0;
+`;
+
+const NavItem = styled.div`
+  ${globalTransition}
+  font-size: 16px;
+  padding: 10px 20px;
+  width: 100%;
+  display: flex;
+  cursor: pointer;
+
+  a {
+    color: black;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+  }
+
+  svg {
+    width: 20px;
+    padding-right: 5px;
+  }
+
+  &:hover {
+    background-color: #d4d4d4;
+    ${globalTransition}
+  }
+
+  &.log-out {
+    display: flex;
+    align-items: center;
+    font-weight: 550;
+  }
+`;
+
+export default function LeftNavBar() {
+  const { user, setUser } = useAuth();
+  const navigate = useNavigate();
+
+  const onLogout = async () => {
+    privateApi
+      .get("/v1/logout")
+      .then(() => {
+        setUser(null);
+        navigate("/login");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  return (
+    <Nav>
+      <NavItem>
+        <Link to="/">
+          <svg
+            fill="none"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
+          </svg>
+          Home
+        </Link>
+      </NavItem>
+      {user?.loggedIn && (
+        <NavItem>
+          <Link to="/profile">
+            <svg
+              fill="none"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            Profile
+          </Link>
+        </NavItem>
+      )}
+      {user?.authorities.includes("ADMIN") && (
+        <NavItem>
+          <Link to="/admin/user">
+            <svg
+              fill="none"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+              />
+            </svg>
+            Admin - User
+          </Link>
+        </NavItem>
+      )}
+      {/* TODO. rm this menu for production */}
+      {user?.authorities.includes("ADMIN") && (
+        <NavItem>
+          <svg
+            role="img"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Swagger</title>
+            <path d="M12 0C5.383 0 0 5.383 0 12s5.383 12 12 12c6.616 0 12-5.383 12-12S18.616 0 12 0zm0 1.144c5.995 0 10.856 4.86 10.856 10.856 0 5.995-4.86 10.856-10.856 10.856-5.996 0-10.856-4.86-10.856-10.856C1.144 6.004 6.004 1.144 12 1.144zM8.37 5.868a6.707 6.707 0 0 0-.423.005c-.983.056-1.573.517-1.735 1.472-.115.665-.096 1.348-.143 2.017-.013.35-.05.697-.115 1.038-.134.609-.397.798-1.016.83a2.65 2.65 0 0 0-.244.042v1.463c1.126.055 1.278.452 1.37 1.629.033.429-.013.858.015 1.287.018.406.073.808.156 1.2.259 1.075 1.307 1.435 2.575 1.218v-1.283c-.203 0-.383.005-.558 0-.43-.013-.591-.12-.632-.535-.056-.535-.042-1.08-.075-1.62-.064-1.001-.175-1.988-1.153-2.625.503-.37.868-.812.983-1.398.083-.41.134-.821.166-1.237.028-.415-.023-.84.014-1.25.06-.665.102-.937.9-.91.12 0 .235-.017.369-.027v-1.31c-.16 0-.31-.004-.454-.006zm7.593.009a4.247 4.247 0 0 0-.813.06v1.274c.245 0 .434 0 .623.005.328.004.577.13.61.494.032.332.031.669.064 1.006.065.669.101 1.347.217 2.007.102.544.475.95.941 1.283-.817.549-1.057 1.333-1.098 2.215-.023.604-.037 1.213-.069 1.822-.028.554-.222.734-.78.748-.157.004-.31.018-.484.028v1.305c.327 0 .627.019.927 0 .932-.055 1.495-.507 1.68-1.412.078-.498.124-1 .138-1.504.032-.461.028-.927.074-1.384.069-.715.397-1.01 1.112-1.057a.972.972 0 0 0 .199-.046v-1.463c-.12-.014-.204-.027-.291-.032-.536-.023-.804-.203-.937-.71a5.146 5.146 0 0 1-.152-.993c-.037-.618-.033-1.241-.074-1.86-.08-1.192-.794-1.753-1.887-1.786zm-6.89 5.28a.844.844 0 0 0-.083 1.684h.055a.83.83 0 0 0 .877-.78v-.046a.845.845 0 0 0-.83-.858zm2.911 0a.808.808 0 0 0-.834.78c0 .027 0 .05.004.078 0 .503.342.826.859.826.507 0 .826-.332.826-.853-.005-.503-.342-.836-.855-.831zm2.963 0a.861.861 0 0 0-.876.835c0 .47.378.849.849.849h.009c.425.074.853-.337.881-.83.023-.457-.392-.854-.863-.854z" />
+          </svg>
+          <a
+            href="http://127.0.0.1:8080/swagger-ui/index.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            OpenAPI
+          </a>
+        </NavItem>
+      )}
+      {user?.loggedIn && (
+        <NavItem className="log-out" onClick={onLogout}>
+          <svg
+            fill="none"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
+            />
+          </svg>
+          Logout
+        </NavItem>
+      )}
+    </Nav>
+  );
+}
