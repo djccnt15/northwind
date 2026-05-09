8f2c25a62516bd6d7339b3b578765866d7255d63
Jay.D <djccnt15@gmail.com>
2026-05-09 10:38:58 +0900

    fix JPA auditing not working bug

--- src/main/java/com/djccnt15/northwind/global/config/jpa/JpaConfig.java
+++ src/main/java/com/djccnt15/northwind/global/config/jpa/JpaConfig.java
@@ -1,7 +1,9 @@
 package com.djccnt15.northwind.global.config.jpa;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Configuration
 @EnableJpaAuditing  // enable @CreatedDate/@LastModifiedDate, @CreatedBy/@LastModifiedBy
 public class JpaConfig {
 }
