015e981e8c9576266072a928267fc2f8c722adb8
Jay.D <djccnt15@gmail.com>
2026-05-23 20:19:54 +0900

    create `TestConst` for constants in test code

---
+++ src/test/java/com/djccnt15/northwind/constants/TestConst.java
@@ -0,0 +1,10 @@
+package com.djccnt15.northwind.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class TestConst {
+    
+    public static final String TEST = "TEST";
+    public static final String SYSTEM = "system";
+}
