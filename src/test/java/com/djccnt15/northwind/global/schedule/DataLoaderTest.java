fb59877b03981e80d300c1e58ea9b4d66f291f2b
Jay.D <djccnt15@gmail.com>
2026-05-24 11:45:47 +0900

    create `DataLoaderTest` for testing `DataLoader`, `DataCacheStorage`

---
+++ src/test/java/com/djccnt15/northwind/global/schedule/DataLoaderTest.java
@@ -0,0 +1,26 @@
+package com.djccnt15.northwind.global.schedule;
+
+import com.djccnt15.northwind.global.storage.DataCacheStorage;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Slf4j
+@SpringBootTest
+class DataLoaderTest {
+    
+    @Autowired private DataLoader dataLoader;
+    @Autowired private DataCacheStorage dataCacheStorage;
+    
+    @Test
+    void refreshSuperAdminCache() {
+        assertDoesNotThrow(() -> dataLoader.refreshSuperAdminCache());
+        
+        var superAdmins = dataCacheStorage.getSuperAdmins();
+        assertNotNull(superAdmins);
+    }
+}
--- src/test/resources/data-h2.sql
+++ src/test/resources/data-h2.sql
@@ -13,3 +13,15 @@ VALUES ('system')
 INSERT INTO title (title)
 VALUES ('system')
 ;
+
+INSERT INTO app_user (username, password, team_id)
+VALUES
+    ('system', 'system', 1),
+    ('admin', 'admin', 1)
+;
+
+INSERT INTO app_user_role (app_user_id, user_role_id)
+VALUES
+    (1, 1),
+    (2, 2)
+;
