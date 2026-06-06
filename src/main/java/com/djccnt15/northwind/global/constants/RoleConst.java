ee8d764a3dab734bca685bddda35fcadbe09ff21
Jay.D <djccnt15@gmail.com>
2026-06-06 21:25:38 +0900

    update permissions doc: change authority for features feat: update role permissions for `product`, `category` domain

--- src/main/java/com/djccnt15/northwind/global/constants/RoleConst.java
+++ src/main/java/com/djccnt15/northwind/global/constants/RoleConst.java
@@ -9,4 +9,10 @@ public class RoleConst {
     public static final String ADMIN = "ADMIN";
     public static final String MANAGER = "MANAGER";
     public static final String USER = "USER";
+    
+    public static final String COMPANY = "COMPANY";
+    public static final String ORDER = "ORDER";
+    public static final String PURCHASE = "PURCHASE";
+    public static final String PRODUCT = "PRODUCT";
+    public static final String STOCK = "STOCK";
 }
